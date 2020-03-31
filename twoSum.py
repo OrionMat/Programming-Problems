a8dccf05b8c47cb48ac8e353d74816db7c1ab2d0
OrionMat <36226295+OrionMat@users.noreply.github.com>
2020-03-31 17:35:08 -0400

    added twoSum performance

--- twoSum.py
+++ twoSum.py
@@ -3,6 +3,7 @@
 # Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
+# faster than 91.50% less memory used than 5.13%
 def onePassHash(nums, target):
     """
     :type nums: List[int]
