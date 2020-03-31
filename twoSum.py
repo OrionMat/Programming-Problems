766a26ce34a00d42e8fe19701ea873093f696fde
OrionMat <36226295+OrionMat@users.noreply.github.com>
2020-03-31 17:18:46 -0400

    Two Sum

---
+++ twoSum.py
@@ -0,0 +1,34 @@
+# Two sum
+
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+def onePassHash(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    dict = {}
+    i = 0
+    for idx, num in enumerate(nums):
+        result = target - num
+        hash_index = dict.get(result)
+        if hash_index and hash_index != idx:
+            return [idx, hash_index]
+        dict[num] = i
+        i +=1
+        return None
+
+def twoPassHash(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    hashtable = {value: index for index, value in enumerate(nums)}
+    for index, value in enumerate(nums):
+        result = target - value
+        hash_index = hashtable.get(result)
+        if hash_index and hash_index != index:
+            return [index, hash_index]
