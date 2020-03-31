6306a069d21e3fc956e570624c36a0af3dd8b17b
OrionMat <36226295+OrionMat@users.noreply.github.com>
2020-03-31 16:39:05 -0400

    is palindrome number

---
+++ isPalindrome.py
@@ -0,0 +1,11 @@
+# Determine whether an integer is a palindrome. 
+# An integer is a palindrome when it reads the same backward as forward.
+
+# faster than 99.49% less memory used than 6.03%
+def isPalindrome(x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    y = str(x)
+    return y == y[::-1]
